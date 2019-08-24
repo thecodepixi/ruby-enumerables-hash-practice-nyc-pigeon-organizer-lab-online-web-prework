80822e89ef68f86156c98c0fe16461ed170cc62e
Emily Harber <emily.a.harber@gmail.com>
2019-08-24 04:19:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
       pigeons.each do |pigeon|
         organized_pigeons[pigeon] = {color: [], gender: [], lives: []}
         binding.pry 
-        organized_pigeons[pigeon][:color] = values[key]
+        organized_pigeons[pigeon][attribute] = data[attribute][values]
       end
     end
   end
